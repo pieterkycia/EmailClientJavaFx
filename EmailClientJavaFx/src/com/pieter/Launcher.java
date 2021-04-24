59d65f591530692e5d8a0e9e5363536b8417dc62
Piotr Kycia <piotr.kycia.programista@gmail.com>
2021-04-24 10:11:39 +0200

    Building the login window

--- EmailClientJavaFx/src/com/pieter/Launcher.java
+++ EmailClientJavaFx/src/com/pieter/Launcher.java
@@ -14,9 +14,9 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("view/first.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
 
-        Scene scene = new Scene(parent, 350, 250);
+        Scene scene = new Scene(parent, 350, 200);
 
         stage.setTitle("");
         stage.setScene(scene);
---
+++ EmailClientJavaFx/src/com/pieter/view/LoginWindowController.java
@@ -0,0 +1,22 @@
+package com.pieter.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void loginButtonAction () {
+        System.out.println("clicked!");
+    }
+}
