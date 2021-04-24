216d0f2ff358cb5fd27dcf9b109fb7b8199a8d33
Piotr Kycia <piotr.kycia.programista@gmail.com>
2021-04-24 11:17:38 +0200

    Initializing stages

--- EmailClientJavaFx/src/com/pieter/Launcher.java
+++ EmailClientJavaFx/src/com/pieter/Launcher.java
@@ -2,9 +2,6 @@ package com.pieter;
 
 import com.pieter.view.ViewFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
--- EmailClientJavaFx/src/com/pieter/view/ViewFactory.java
+++ EmailClientJavaFx/src/com/pieter/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.pieter.view;
 
 import com.pieter.EmailManager;
+import com.pieter.controller.BaseController;
+import com.pieter.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -12,5 +20,23 @@ public class ViewFactory {
 
     public void showLoginWindow() {
         System.out.println("Show login window");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+
+        Parent parent;
+
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
