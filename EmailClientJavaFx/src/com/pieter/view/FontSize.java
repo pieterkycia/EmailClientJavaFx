6b0953c15bc4b8245ec69770c87ce81b8fc341eb
Piotr Kycia <piotr.kycia.programista@gmail.com>
2021-04-24 12:42:48 +0200

    Updating the css dynamically

--- EmailClientJavaFx/src/com/pieter/view/FontSize.java
+++ EmailClientJavaFx/src/com/pieter/view/FontSize.java
@@ -3,5 +3,18 @@ package com.pieter.view;
 public enum FontSize {
     SMALL,
     MEDIUM,
-    BIG
+    BIG;
+
+    public static String getCssPath(FontSize fontSize) {
+        switch (fontSize) {
+            case SMALL:
+                return "css/fontSmall.css";
+            case MEDIUM:
+                return "css/fontMedium.css";
+            case BIG:
+                return "css/fontBig.css";
+            default:
+                return null;
+        }
+    }
 }
