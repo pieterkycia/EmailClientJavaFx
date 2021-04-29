14bced48ca196e95b99a65e8ec9cf30ffef888dd
Piotr Kycia <piotr.kycia.programista@gmail.com>
2021-04-29 15:19:02 +0200

    Folders icon

--- EmailClientJavaFx/src/com/pieter/controller/services/FetchFoldersService.java
+++ EmailClientJavaFx/src/com/pieter/controller/services/FetchFoldersService.java
@@ -1,6 +1,7 @@
 package com.pieter.controller.services;
 
 import com.pieter.model.EmailTreeItem;
+import com.pieter.view.IconResolver;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
@@ -18,6 +19,7 @@ public class FetchFoldersService extends Service<Void> {
     private Store store;
     private EmailTreeItem<String> foldersRoot;
     private List<Folder> folderList;
+    private IconResolver iconResolver = new IconResolver();
 
     public FetchFoldersService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store = store;
@@ -45,6 +47,7 @@ public class FetchFoldersService extends Service<Void> {
         for (Folder folder : folders) {
             folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
+            emailTreeItem.setGraphic(iconResolver.getIconFolder(folder.getName()));
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
             fetchMessagesOnFolder(folder, emailTreeItem);
